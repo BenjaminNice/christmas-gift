8d6e206bdb2ac4a6892d9acbf569ebb50c2e4061
BenjaminNice <benjaminnice000@gmail.com>
2024-12-23 16:39:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let currentScene = 0;
+
+function nextScene() {
+    const scenes = document.querySelectorAll(".scene");
+    if (currentScene < scenes.length - 1) {
+        scenes[currentScene].classList.remove("active");
+        scenes[currentScene].classList.add("hidden");
+        currentScene++;
+        scenes[currentScene].classList.remove("hidden");
+        scenes[currentScene].classList.add("active");
+    }
+}
+
+function showGift() {
+    const gift = document.getElementById("gift");
+    gift.classList.remove("hidden");
+    gift.classList.add("active");
+}
 
